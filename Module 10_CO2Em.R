51ffae993f223cfbe0da8d6b077116ee5a281ebc
Zhong Zheng <34171353+3zzheng@users.noreply.github.com>
2022-03-27 20:23:38 -0400

    Add files via upload

---
+++ Module 10_CO2Em.R
@@ -0,0 +1,13 @@
+#Package: CO2Em
+#Type: Package
+#Title: Analysis of Worldwide CO2 Emissions
+#Version: 0.01
+#Authors: Zhong Zheng
+#Maintainer: 3zzheng@gmail.com
+#Description: Users can analyze the global trend of CO2 Emissions and its associating factors 
+              #including GDP per capita and educational attainment by countries/economies. 
+              #Data are collected by the World Bank. Both descriptive and inferntial statistics 
+              #are available.
+#Depends: R (>= 3.1.2)
+#License: CC0
+#LazyData: True
